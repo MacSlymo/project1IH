60557581d104b7d2fa83aa4a4dec5a18310c4b68
Mathieu Revillion <mathieu.revillion@gmail.com>
2017-07-08 19:43:05 +0200

    added more parameters + drawACard method

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ What is the data I need ? How to store it ?
 I thought there would be 3 classes : board, dealer and players (1 or 2).
 
 In the board, the first thing I needed was a deck of cards. I thought first to create manually an array of objects, but then I found on the internet the idea to make a constructor function to generate the deck. Less writings here. I chose this idea.
+
+Checked if all the files were properly linked to the HTML.
+
+Then I started to create the game constructor itself. This is the most difficult part for me, so I wanted to dive in as soon as possible.
+
+Structure (parameters of the constructor) is what is present on the game board. I added them to the constructor,  thanks to "this.".
+
+Then the first method ... drawing a random card among the deck. Easy with the "sample" function from lodash.
+
+But then I had to make sure these cards were no longer available in the deck, in order to maintain the probabilities, essential in this game.
+
+Developed the function to draw a new random card from the deck, and remove it from the deck.
--- project1/js/board.js
+++ project1/js/board.js
@@ -1,4 +1,5 @@
 //Created a deck of cards using constructor
+//(good training for constructors and loops)
 
 function card(value, name, suit){
 	this.value = value;
@@ -6,19 +7,69 @@ function card(value, name, suit){
 	this.suit = suit;
 }
 
-function deck(){
+function deckOfCards(){
 	this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 	this.suits = ['Hearts','Diamonds','Spades','Clubs'];
 	var cards = [];
 
-    for( var s = 0; s < this.suits.length; s++ ) {
-        for( var n = 0; n < this.names.length; n++ ) {
-            cards.push( new card( n+1, this.names[n], this.suits[s] ) );
-        }
-    }
+		for( var s = 0; s < this.suits.length; s++ ) {
+				for( var n = 0; n < this.names.length; n++ ) {
+						cards.push( new card( n+1, this.names[n], this.suits[s] ) );
+						}
+				}
+				return cards;
+		}
 
-    return cards;
+
+//Created the proper game constructor
+
+function GameBlackJack () {
+	this.turn = 0;
+  this.deck = new deckOfCards();
+	this.cardsBin = [];
+	this.availableCards = [_.remove(this.deck, function(){
+		var that = this;
+		return that.cardsBin;
+	})];
+	this.dealerHand = [];
+	this.player1Hand = [];
+	this.player2Hand = [];
+	this.player1Chips = 50;
+	this.player2Chips = 50;
+	this.player1Pot = 0;
+	this.player2Pot = 0;
+  this.chips = {
+		chipOf1 : 1,
+		chipOf2 : 2,
+		chipOf5 : 5,
+		chipOf10 : 10
+	};
+
+//REMOVE THE DRAWN CARDS FROM DECK
+
+	GameBlackJack.prototype.takeACard = function () {
+		var that = this;
+		var newCard = _.sample(that.deck);
+		that.cardsBin.push(newCard);
+		return newCard;
+	};
+
+/*
+	 GameBlackJack.prototype.bet= function () {
+		$(document).onClick('.chip', function() {
+    	var pot = $('.chip'.val());
+		});
+		return pot;
+	};
+
+	GameBlackJack.prototype.pass= function () {
+
+	};
+*/
+
+
 }
 
-var newDeck = new deck();
-console.log(newDeck);
+var game = new GameBlackJack();
+
+console.log(game);
